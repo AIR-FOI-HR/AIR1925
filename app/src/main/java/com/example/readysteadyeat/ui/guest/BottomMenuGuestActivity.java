6bd873c7c2e944c431aa05bfabfffd0e5181bf37
ilevak <ivalevak@hotmail.com>
2019-12-16 19:01:25 +0100

    Izrada BottomMenia na HomeActivity za profil gosta

---
+++ app/src/main/java/com/example/readysteadyeat/ui/guest/BottomMenuGuestActivity.java
@@ -0,0 +1,80 @@
+package com.example.readysteadyeat.ui.guest;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.example.readysteadyeat.R;
+import com.example.readysteadyeat.ui.guest.Orders.OrdersGuestFragment;
+import com.example.readysteadyeat.ui.guest.myProfile.ProfileGuestFragment;
+import com.example.readysteadyeat.ui.guest.restaurants.RestaurantListViewFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class BottomMenuGuestActivity extends AppCompatActivity  {
+    BottomNavigationView bottomNavigationView;
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_menu_guest);
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+
+
+        bottomNavigationView.setSelectedItemId(R.id.navigation_orders_guest);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+
+        toolbar=(Toolbar)findViewById(R.id.toolbarBottomGuest);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Orders");
+
+
+
+    }
+
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment=null;
+            toolbar=(Toolbar)findViewById(R.id.toolbarBottomGuest);
+            switch(menuItem.getItemId()){
+                case R.id.navigation_profile_guest:
+                //getSupportFragmentManager().beginTransac-tion().replace(R.id.container, profileGuestFragment).commit();:
+                    selectedFragment=new ProfileGuestFragment();
+                    setSupportActionBar(toolbar);
+                    getSupportActionBar().setTitle("Profile");
+
+                break;
+
+                case R.id.navigation_orders_guest:
+                //getSupportFragmentManager().beginTransaction().replace(R.id.container, ordersGuestFragment).commit();
+                    selectedFragment=new OrdersGuestFragment();
+                    setSupportActionBar(toolbar);
+                    getSupportActionBar().setTitle("Orders");
+                break;
+
+                case R.id.navigation_list_guest:
+                //getSupportFragmentManager().beginTransaction().replace(R.id.container, restaurantListViewFragment).commit();
+                    selectedFragment=new RestaurantListViewFragment();
+                    setSupportActionBar(toolbar);
+                    getSupportActionBar().setTitle("Restaurants");
+                break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.container,selectedFragment ).commit();
+            return true;
+        }
+    };
+
+
+
+
+}
